3e69a0aa39655726db47d291af68eb33ab9904ce
Rodrigo <rodrigofreites1@gmail.com>
2022-04-21 15:07:15 -0300

    Layout Push

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
 import { Typography } from '@mui/material';
 import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>AF Juegos Digitales</title>
-        <meta name="description" content="Tienda de juegos digitales" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant="h1">
-        AF Juegos Digitales
-      </Typography>
-    </div>
-  );
+  return <Layout>List Products</Layout>;
 }
