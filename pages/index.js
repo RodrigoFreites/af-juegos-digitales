897d5dc289c7a758845538548e594f78cdc1a607
Rodrigo <rodrigofreites1@gmail.com>
2022-04-25 11:44:29 -0300

    SanityChanges

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-import { Typography } from '@mui/material';
-import Head from 'next/head';
+import { Alert, CircularProgress, Typography, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
+import ProdcutItem from '../components/ProdcutItem';
+import client from '../utils/client';
 
 export default function Home() {
-  return <Layout>List Products</Layout>;
+  const [state, setState] = useState({
+    products: [],
+    error: '',
+    loading: true,
+  });
+  const { loading, error, products } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await client.fetch(`*[_type == "product"]`);
+        setState({ products, loading: false });
+      } catch (err) {
+        setState({ loading: false, error: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Layout>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant="danger">{error}</Alert>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.slug}>
+              <ProdcutItem product={product}></ProdcutItem>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Layout>
+  );
 }
---
+++ utils/config.js
@@ -0,0 +1,6 @@
+const config = {
+  projectId: '7j7nj6vw',
+  dataset: 'production',
+};
+
+export default config;
